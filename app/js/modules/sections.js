426416a7a27d5c362677b4b590a77154c1447a0b
davidoff0895 <evgeniy@MacBook-Pro-Evgenij.local>
2022-05-02 22:46:06 +0300

    added news section

--- app/js/modules/sections.js
+++ app/js/modules/sections.js
@@ -1,3 +1,3 @@
 const overlayElements = document.getElementsByClassName('section__pattern__overlay');
 Array.prototype.forEach.call(overlayElements,((overlay, i) =>
-    overlay.style.backgroundImage = `url(/img/blog/electronics/${i+1}.jpg)`))
+    overlay.style.backgroundImage = `url(/img/blog/sections/${i+1}.jpg)`));
